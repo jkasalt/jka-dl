83e9fbf897fc90217f331664b885d3de17d8660c
Luca Bracone <lucabr123@gmail.com>
2021-03-02 14:33:36 +0100

    first commit

---
+++ week1.py
@@ -0,0 +1,51 @@
+import torch
+import time
+
+x = torch.full((13,13),1)
+x[:, 1:12:5] = 2
+x[1:12:5, :] = 2
+x[3:5, 3:5] = 3
+x[8:10, 8:10] = 3
+x[3:5, 8:10] = 3
+x[8:10, 3:5] = 3
+# ===========================================
+m = torch.empty((20,20)).normal_()
+d = torch.diag(torch.arange(20.0))
+m_inv = torch.inverse(m)
+
+p1 = torch.matmul(m_inv, d)
+p = torch.matmul(p1, m)
+e = torch.eig(p)
+# =========================================
+start_time = time.perf_counter()
+A = torch.empty(5000,5000).normal_()
+B = torch.empty(5000,5000).normal_()
+C = torch.mm(A,B)
+end_time = time.perf_counter()
+# =========================================
+def mul_row(T):
+    output = torch.empty(T.size())
+    num_lines = T.size()[0]
+    num_cols = T.size()[1]
+    for n, i in enumerate(range(num_lines)):
+        n += 1
+        for j in range(num_cols):
+            output[i,j] = n * T[i,j]
+    return output
+
+def mul_row_fast(T):
+    num_lines = float(T.size()[0])
+    mul_vals = torch.diag(torch.arange(1, num_lines+1))
+    output = torch.mm(mul_vals, T)
+    return output
+
+m = torch.full((1000, 400), 2.0)
+start_time = time.perf_counter()
+print(mul_row(m))
+end_time = time.perf_counter()
+print("slow:", end_time - start_time)
+
+start_time = time.perf_counter()
+print(mul_row_fast(m))
+end_time = time.perf_counter()
+print("fast:", end_time - start_time)
---
+++ week2.py
@@ -0,0 +1,6 @@
+import torch
+from torch import Tensor
+import dlc_practical_prologue as prologue
+
+def nearest_classification(train_input, train_target, x):
+    
